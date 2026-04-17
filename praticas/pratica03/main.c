8b2e0c5e3d4929b73f674ed815e2535cb9dacb01
Giovanni <giominuzzi12@gmail.com>
2026-04-17 11:07:38 -0300

    feat: conclui pratica03. Fecha #7

--- praticas/pratica03/main.c
+++ praticas/pratica03/main.c
@@ -9,7 +9,7 @@ int main() {
     contaDepositar(&conta, 150.0);
     printf("Pos-deposito de R$150.0, valor atual: R$ %.2f\n => %d", contaSaldo(&conta), contaSaldo(&conta)==650.0);
 
-    contaSacar(&conta, 100.0)
+    contaSacar(&conta, 100.0);
     printf("Pos-saque de 100.0, valor atual: R$ %.2f\n => %d", contaSaldo(&conta), contaSaldo(&conta)==550.0);
 
     contaDestruir(&conta);
